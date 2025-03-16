58ba1dc20af8883aa1a6a1e3c06dd3f168fb4136
Jennifer Patel <jennifer@goinvo.com>
2025-03-16 16:55:15 -0400

    config for gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/jens-portfolio-2024/' : '',
+  basePath: isProd ? '/jens-portfolio-2024' : '',
+  output: 'export',
+
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
