2c409c5a1b34228d2ef520fa0d2e33fe99381c38
Jennifer Patel <jennifer@goinvo.com>
2026-02-26 14:50:05 -0500

    contact box icons

--- src/components/contact.js
+++ src/components/contact.js
@@ -17,14 +17,26 @@ const Contact = () => {
                 className="button button-white button-big"
                 >
                 <span className="inline-block">
+                    <span className="icon email"></span>
+                </span>
+                <span className="inline-block">
                     <span className="block">Email</span>
                     <span className="block text-small">jennifer.c.patel@gmail.com</span>
                 </span>
+            </a>
+            <a
+                href="www.linkedin.com/in/jennifer-patel-sobus"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="button button-white button-big"
+                >
                 <span className="inline-block">
-                    <span className="icon email"></span>
+                    <span className="icon linkedin"></span>
                 </span>
-                
-                
+                <span className="inline-block">
+                    <span className="block">LinkedIn</span>
+                    <span className="block text-small">Connect with me</span>
+                </span>
             </a>
             <a
                 href="www.linkedin.com/in/jennifer-patel-sobus"
@@ -32,18 +44,12 @@ const Contact = () => {
                 rel="noopener noreferrer"
                 className="button button-white button-big"
                 >
-                <span className="block">LinkedIn</span>
-                <span className="block text-small">Connect with me</span>
-                <span className="icon linkedin"></span>
-            </a>
-            <a
-                href="www.linkedin.com/in/jennifer-patel-sobus"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="button button-white button-big"
-                >
-                <span className="block">Github</span>
-                <span className="icon github"></span>
+                <span className="inline-block">
+                    <span className="icon github"></span>
+                </span>
+                <span className="inline-block">
+                    <span className="block">Github</span>
+                </span>
             </a>
         </div>
   </div>
